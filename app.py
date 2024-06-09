3c70ec9c7f68b3379a6229059c3e53711f4c55b0
Juliette Bassnagel <j.bassnagel@gmail.com>
2024-06-09 17:48:08 +0200

    REF: split app introduction and wimlds presentation

--- app.py
+++ app.py
@@ -2,19 +2,22 @@ import gradio as gr
 from main import main
 
 wimlds_presentation = (
+    "WiMLDS aims to support and promote women and gender minorities who are practicing, studying, or have an "
+    "interest in machine learning and data science."
+)
+
+title_and_intro = (
     "# Women in Machine Learning and Data Science Researchers Project \n \n "
     "This tool helps you find the latest research papers in the field of data science that are written or co-written "
     "by at least one woman. \n \n "
-    "WiMLDS aims to support and promote women and gender minorities who are practicing, studying, or have an "
-    "interest in machine learning and data science."
-
 )
 
 with gr.Blocks() as papers:
-    gr.Markdown(wimlds_presentation)
+    gr.Markdown(title_and_intro)
     inp = gr.Radio(choices=["Arxiv", "Hugging Face Selection"], value="Arxiv", label="Source")
     btn = gr.Button("Search")
     out = gr.HTML()
+    gr.Markdown(wimlds_presentation)
     btn.click(fn=main, inputs=inp, outputs=out)
 
 papers.launch(inbrowser=True)
